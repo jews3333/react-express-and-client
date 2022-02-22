47ad923f94479a96d1dcd5cf3eda75f1de3c1a7c
jews3333 <jews333333@gmail.com>
2022-02-22 17:53:57 +0900

    build

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname+"/client/build/index.html"));
-})
+});
 
 
 console.log(`server running at http ${port}`);
